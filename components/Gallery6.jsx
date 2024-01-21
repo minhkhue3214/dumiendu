2dcc6d15ec5b8028cecb564d0f65399311884eec
minhkhue3214 <dominh3214@gmail.com>
2024-01-21 07:44:03 +0700

    update test

--- components/Gallery6.jsx
+++ components/Gallery6.jsx
@@ -4,12 +4,12 @@ const Gallery6 = () => {
     return (
             <div className="gallery-wrap gallery-wrap--dense">
             <div className="gallery gallery--stack gallery--stack-inverse gallery--stack-scale gallery--stack-dark" id="gallery-6">
-                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/07.jpg)' }}></div>
-                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/08.jpg)' }}></div>
-                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/09.jpg)' }}></div>
-                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/10.jpg)' }}></div>
-                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/03.jpg)' }}></div>
-                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/05.jpg)' }}></div>
+                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/07.webp)' }}></div>
+                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/08.webp)' }}></div>
+                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/09.webp)' }}></div>
+                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/10.webp)' }}></div>
+                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/03.webp)' }}></div>
+                <div className="gallery__item" style={{ backgroundImage: 'url(/look01/05.webp)' }}></div>
                 <div className="caption">
                     <p>This uncharted territory challenges artists and art enthusiasts alike, igniting debates about the role of intention and chance in the artistic process.</p>
                 </div>
